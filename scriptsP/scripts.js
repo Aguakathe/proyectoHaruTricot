572dfcbb92b006ac07a47480141545d469add288
Aguakathe <sktorres6@gmail.com>
2025-04-29 19:13:09 -0500

    Lo moví a una nueva carpeta

---
+++ scriptsP/scripts.js
@@ -0,0 +1,8 @@
+
+// Menu hamburguesa 
+  const toggle = document.getElementById('menu-toggle');
+  const navbar = document.getElementById('navbar');
+
+  toggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+  });
