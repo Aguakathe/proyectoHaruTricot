8992449815285dcb029a00441867cb28783f6bc9
Aguakathe <sktorres6@gmail.com>
2025-04-29 20:11:19 -0500

    anadí y modifique el formulario

--- scripts.js
+++
@@ -1,8 +0,0 @@
-
-// Menu hamburguesa 
-  const toggle = document.getElementById('menu-toggle');
-  const navbar = document.getElementById('navbar');
-
-  toggle.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-  });
